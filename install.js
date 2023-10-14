6e061e9bf5e9a70aaedf00dc670fbfb8dc5be021
911218sky <3414061123aa@gmail.com>
2023-10-15 03:04:09 +0800

    Changes to be committed: 	modified:   install.bat 	new file:   install.js

---
+++ install.js
@@ -0,0 +1,39 @@
+const { execSync } = require('child_process');
+
+// Check if Node.js is installed
+try {
+  execSync('node -v', { stdio: 'ignore' });
+} catch (error) {
+  console.error('Node.js is not installed. Please install Node.js first.');
+  process.exit(1);
+}
+
+// Check if TypeScript is installed
+try {
+  execSync('tsc -v', { stdio: 'ignore' });
+  console.log('TypeScript (tsc) is already installed.');
+} catch (error) {
+  console.log('TypeScript (tsc) is not installed. Attempting to install TypeScript.');
+
+  // Install TypeScript globally using npm
+  try {
+    execSync('npm install -g typescript', { stdio: 'inherit' });
+    console.log('TypeScript (tsc) installed successfully.');
+  } catch (error) {
+    console.error('TypeScript (tsc) installation failed. Please install TypeScript manually.');
+    process.exit(1);
+  }
+}
+
+// Execute the npm install command
+console.log('Executing the npm install command...');
+try {
+  execSync('npm install', { stdio: 'inherit' });
+  console.log('npm install command executed successfully.');
+} catch (error) {
+  console.error('npm install command failed.');
+  process.exit(1);
+}
+
+// Execute other initialization steps
+// You can add any other necessary initialization steps here
