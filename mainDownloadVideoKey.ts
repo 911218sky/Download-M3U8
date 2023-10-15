5109fc38f88aca46fef2d1b4c45e44ff40ec85ee
911218sky <3414061123aa@gmail.com>
2023-10-15 12:31:54 +0800

    Changes to be committed: 	modified:   downloadData.json 	modified:   mainDownloadVideoKey.ts

--- mainDownloadVideoKey.ts
+++ mainDownloadVideoKey.ts
@@ -7,6 +7,11 @@ interface DownloadData {
   name: string | undefined;
 }
 
+interface JsonData {
+  downloadData: DownloadData[];
+  hasKey: boolean;
+}
+
 const options: AxiosRequestConfig = {
   headers: {
     "user-agent":
@@ -20,34 +25,34 @@ const options: AxiosRequestConfig = {
   },
 };
 
-const downloadData: DownloadData[] = [
-  {
-    url: "https://bahamut.akamaized.net/1133994d9f2be7f63d4f4ecd26e3da8071af7b60/1080p/hdntl=exp=1697389226~acl=%2f*~data=hdntl,a3414061%3a35491%3a1%3a1%3a68686826~hmac=5e30b0bc655a3c32bc9a0e602b498299de07c21f0959f304eb3a38dcb1ec9e87/chunklist_b5000000.m3u8",
-    name: "家裡蹲吸血姬的鬱悶 [2]",
-  },
-];
-
-async function main() {
-  for (const { url, name } of downloadData) {
-    await startDownload(url, options, name || "", {
-      dir: path.join(__dirname, `${name}`),
-      limit: 50,
-      hasKey: true,
-    });
-  }
-}
+// const downloadData: DownloadData[] = [
+//   {
+//     url: "https://bahamut.akamaized.net/1133994d9f2be7f63d4f4ecd26e3da8071af7b60/1080p/hdntl=exp=1697389226~acl=%2f*~data=hdntl,a3414061%3a35491%3a1%3a1%3a68686826~hmac=5e30b0bc655a3c32bc9a0e602b498299de07c21f0959f304eb3a38dcb1ec9e87/chunklist_b5000000.m3u8",
+//     name: "家裡蹲吸血姬的鬱悶 [2]",
+//   },
+// ];
 
 // async function main() {
-//   const downloadData = readDownloadData(
-//     path.join(__dirname, "downloadData.json")
-//   ) as DownloadData[];
 //   for (const { url, name } of downloadData) {
 //     await startDownload(url, options, name || "", {
 //       dir: path.join(__dirname, `${name}`),
 //       limit: 50,
-//       hasKey: false,
+//       hasKey: true,
 //     });
 //   }
 // }
+
+async function main() {
+  const jsonData = readDownloadData(
+    path.join(__dirname, "downloadData.json")
+  ) as JsonData;
+  for (const { url, name } of jsonData.downloadData) {
+    await startDownload(url, options, name || "", {
+      dir: path.join(__dirname, `${name}`),
+      limit: 50,
+      hasKey: jsonData.hasKey,
+    });
+  }
+}
 
 main();
